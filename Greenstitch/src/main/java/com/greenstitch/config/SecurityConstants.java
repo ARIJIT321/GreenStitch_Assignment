cda54e7442a15afa74c250a44e7a6aa97b2df3de
Arijit Bhatta <arijitbhatta123@gmail.com>
2023-07-12 23:32:16 +0530

    made final changes

--- Greenstitch/src/main/java/com/greenstitch/config/SecurityConstants.java
+++ Greenstitch/src/main/java/com/greenstitch/config/SecurityConstants.java
@@ -1,6 +1,7 @@
 package com.greenstitch.config;
 
-public class SecurityConstants {
+
+public interface SecurityConstants {
 	
 	public static final String JWT_KEY = "secretsfhsfjhdjkhgdfjdlkfjsdkffjsd";
 	public static final String JWT_HEADER = "Authorization";
