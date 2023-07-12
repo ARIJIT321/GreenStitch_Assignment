1b7c8a10d286f8e1787216c157223e513ae72fc0
Arijit Bhatta <arijitbhatta123@gmail.com>
2023-07-12 22:45:34 +0530

    created configuration classes

---
+++ Greenstitch/src/main/java/com/greenstitch/config/SecurityConstants.java
@@ -0,0 +1,8 @@
+package com.greenstitch.config;
+
+public class SecurityConstants {
+	
+	public static final String JWT_KEY = "secretsfhsfjhdjkhgdfjdlkfjsdkffjsd";
+	public static final String JWT_HEADER = "Authorization";
+
+}
